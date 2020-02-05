c41c1e7c1354a0f93babaaee11d4f3f55323a26c
Soro Didiouf <ssoro013@gmail.com>
2020-02-05 15:54:44 -0800

    Linted

--- client/components/ReviewSearch.jsx
+++ client/components/ReviewSearch.jsx
@@ -2,43 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Input = styled.input `
-  	border: .5px solid;
-  	outline-color: rgb(0, 132, 137);
-  	width: 150px;
-  	height: 30px;
-  	border-radius: 5px;
+    border: .5px solid;
+    outline-color: rgb(0, 132, 137);
+    width: 150px;
+    height: 30px;
+    border-radius: 5px;
 `;
 
 class ReviewSearch extends React.Component {
-  	constructor(props) {
-		super(props);
-		this.state = {
-	  		value: '',
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-  	}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  	handleChange(event) {
-		this.setState({value: event.target.value});
-  	}
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
 
-  	//function to handle search on submit
-  	handleSubmit(event) {
-		event.preventDefault();
-		this.props.handleSearch(this.state.value);
-  	}
+    //function to handle search on submit
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.handleSearch(this.state.value);
+    }
 
-  	render() {
-		return (
-	  		<div>
-				<form onSubmit={this.handleSubmit}>
-		  			<Input type="search" placeholder=" 🔍 Search Reviews" value={this.state.value} onChange={this.handleChange}></Input>
-				</form>
-	  		</div>
-		);
-  	}
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <Input type="search" placeholder=" 🔍 Search Reviews" value={this.state.value} onChange={this.handleChange}>
+                    </Input>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default ReviewSearch;
