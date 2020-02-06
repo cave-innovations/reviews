e902b0d214aebef8dcd65228f705daab800f4edd
Soro Didiouf <ssoro013@gmail.com>
2020-02-05 16:02:18 -0800

    Linted

--- client/components/Ratings.jsx
+++ client/components/Ratings.jsx
@@ -23,27 +23,27 @@ const Block = styled.div `
 const Ratings = (props) => {
     const ratings1 = [];
     const ratings2 = [];
+
+    //average rating of each metric
     const accuracy = props.ratings.map(a => a.Rating_Accuracy).reduce((a, b) => a + b) / props.ratings.length;
     const checkin = props.ratings.map(a => a.Rating_CheckIn).reduce((a, b) => a + b) / props.ratings.length;
     const cleanliness = props.ratings.map(a => a.Rating_Cleanliness).reduce((a, b) => a + b) / props.ratings.length;
     const communication = props.ratings.map(a => a.Rating_Communication).reduce((a, b) => a + b) / props.ratings.length;
     const location = props.ratings.map(a => a.Rating_Location).reduce((a, b) => a + b) / props.ratings.length;
     const value = props.ratings.map(a => a.Rating_Value).reduce((a, b) => a + b) / props.ratings.length;
+
+    //metrics are divided into two categories
     ratings1.push({accuracy}, {communication}, {cleanliness});
     ratings2.push({location}, {checkin}, {value});
 
     return (
         <Block>
-        <Rating1>
-                {ratings1.map((rating, i) =>
-            <Rating key={i} rating={rating}/>
-            )}
-        </Rating1>
-        <Rating2>
-            {ratings2.map((rating, i) =>
-            <Rating key={i} rating={rating}/>
-            )}
-        </Rating2>
+            <Rating1>
+                {ratings1.map((rating, i) => <Rating key={i} rating={rating}/>)}
+            </Rating1>
+            <Rating2>
+                {ratings2.map((rating, i) => <Rating key={i} rating={rating}/>)}
+            </Rating2>
         </Block>
     );
 };
